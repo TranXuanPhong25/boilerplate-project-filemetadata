0668e3626d1421fa6f84ee5ade9c172f9f3dcb43
TranXuanPhong25 <89262558+TranXuanPhong25@users.noreply.github.com>
2024-07-31 18:19:07 +0700

    chore:done

--- index.js
+++ index.js
@@ -1,16 +1,22 @@
-var express = require('express');
-var cors = require('cors');
+const express = require('express');
+const cors = require('cors');
 require('dotenv').config()
 
-var app = express();
-
+const app = express();
+const multer = require('multer');
 app.use(cors());
 app.use('/public', express.static(process.cwd() + '/public'));
 
 app.get('/', function (req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
-
+app.post('/api/fileanalyse', multer().single('upfile'), (req, res) => {
+  res.json({
+    name: req.file.originalname,
+    type: req.file.mimetype,
+    size: req.file.size
+  })
+});
 
 
 
